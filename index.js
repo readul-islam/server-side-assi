19d3c23f4df60d50d16b0c01ee5c60e379bf010f
readul-islam <developer.readul@gmail.com>
2022-05-22 20:09:08 +0600

    create products api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors= require('cors');
@@ -16,6 +16,21 @@ const run =async()=>{
         await client.connect();
         const productCollection = client.db("manufacture").collection("products");
         console.log('hellow worlkd');
+        app.get('/products',async(req, res)=>{
+
+            const query = {};
+            const cursor = productCollection.find(query);
+          const result = await cursor.toArray()
+          res.send(result)
+        })
+        app.get('/products/:id',async(req, res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const query ={_id:ObjectId(id)}
+            const result = await productCollection.findOne(query);
+             res.send(result)
+
+        })
 
 
     }
