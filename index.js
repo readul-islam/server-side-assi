9332b7d27c946d933ed81e677e9765cc95b75541
readul-islam <developer.readul@gmail.com>
2022-05-25 21:22:46 +0600

    create review api

--- index.js
+++ index.js
@@ -1,179 +1,180 @@
 const express = require('express');
 var jwt = require('jsonwebtoken');
+
+//stripe key 
 const stripe = require("stripe")('sk_test_51L1AL5E24HHvYt8NXQn1l7Wichrhs2EzUaMZxJkATi12Gdq2EEzFyZZ8YFMf8Pq5dXr1GdSgoOC2Do21V0VlcBDt00HtDXydNz');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-const cors= require('cors');
+const cors = require('cors');
 const productRoutes = require('./routes/Products');
 // const { default: Stripe } = require('stripe');
 
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const jwtToken = (req,res,next) => {
-   const authHeader = req.headers.Authorization
-   if(!authHeader){
-       res.status(401).send('massage:Undathorized')
-   }else{
-       const [Breare,token] = authHeader.split(" ")
-    jwt.verify(token, process.env.ACESS_TOKEN, function(err, decoded) {
-        if(err){
-            res.statue(401).send({massage:'forbidden access'})
-        }else{
-            decoded = req.decoded
+const jwtToken = (req, res, next) => {
+  const authHeader = req.body.headers.Authorization;
+  //  console.log(authHeader)
+  if (!authHeader) {
+    res.send('massage:Undathorized')
+  } else {
+    const token = authHeader.split(' ')[1]
+    console.log(token);
+    console.log(token);
+    jwt.verify(token, process.env.ACESS_TOKEN, function (err, decoded) {
+      if (err) {
+        res.send({ massage: 'forbidden access' })
+      } else {
+        decoded = req.decoded
 
-            next()
-        }
-      });
-   }
+        next()
+      }
+    });
+  }
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bcfms.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const run =async()=>{
-    try{
-        await client.connect();
-        const productCollection = client.db("manufacture").collection("products");
-        const orderCollection = client.db("manufacture").collection("orders");
-        const reviewCollection = client.db("manufacture").collection("reviews");
-        const userCollection = client.db("manufacture").collection("users");
-        console.log('hellow worlkd');
-        app.get('/products',async(req, res)=>{
+const run = async () => {
+  try {
+    await client.connect();
+    const productCollection = client.db("manufacture").collection("products");
+    const orderCollection = client.db("manufacture").collection("orders");
+    const reviewCollection = client.db("manufacture").collection("reviews");
+    const userCollection = client.db("manufacture").collection("users");
+    const paymentCollection = client.db("manufacture").collection("payments");
 
-            const query = {};
-            const cursor = productCollection.find(query);
-          const result = await cursor.toArray()
-          res.send(result)
-        })
-       
-        app.get('/products/:id',async(req, res)=>{
-            const id = req.params.id;
-            console.log(id);
-           
-            const query ={_id:ObjectId(id)}
-            const result = await productCollection.findOne(query);
-             res.send(result)
+    app.get('/products', async (req, res) => {
 
-        })
-        // app.put('/products/:id',async(req, res)=>{
-        //     const id = req.params.id;
-           
-        //     const query ={_id:ObjectId(id)}
-        //     const result = await productCollection.findOne(query);
-        //      res.send(result)
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
-        // })
-        // -------------------
-        app.post('/review',async(req, res)=>{
-            const review =req.body;
-            console.log(review);
-            const result = await reviewCollection.insertOne(review);
-            res.send(result);
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
 
-        })
 
-        
-           
-           
+      const query = { _id: ObjectId(id) }
+      const result = await productCollection.findOne(query);
+      res.send(result)
 
+    })
+
+    // -------------------
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+
+    })
+    app.get('/review', async (req, res) => {
+
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
+    })
     //    -------------------------------
-    app.get('/order', async (req, res)=>{
-        const query ={}
-        const cursor = orderCollection.find(query);
-          const result = await cursor.toArray()
-          res.send(result)
+    app.get('/order/:id', async (req, res) => {
+      const email = req.params.id
+      console.log(email);
+      const query = {email:email}
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
 
 
     })
-    app.delete('/order/:id', async (req, res)=>{
-        const id = req.params.id;
-           
-            const query ={_id:ObjectId(id)}
-            const result = await orderCollection.deleteOne(query);
-          res.send(result)
-})
-    app.get('/order/:id', async (req, res)=>{
-        const id = req.params.id;
-        // console.log(id);
-           
-            const query ={_id:ObjectId(id)}
-            const result = await orderCollection.findOne(query);
-          res.send(result)
-})
-app.patch('/order/:id', async (req, res)=>{
-  const id = req.params.id;
-  const filter ={_id:ObjectId(id)}
-  const updateDoc = {
-    $set: {
-      paid:true,
-      tran
-    },
-  };
-  const result = await movies.updateOne(filter, updateDoc, options);
-  
-})
-        
-    app.post('/order',async(req, res)=>{
-            const ordered = (req.body.orderInfo);
-           
-            const result = await orderCollection.insertOne(ordered);
-            res.send(result);
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
 
-        })
+      const query = { _id: ObjectId(id) }
+      const result = await orderCollection.deleteOne(query);
+      res.send(result)
+    })
+    app.get('/order/:id', async (req, res) => {
+      const id = req.params.id;
 
 
+      const query = { _id: ObjectId(id) }
+      const result = await orderCollection.findOne(query);
+      res.send(result)
+    })
+    app.patch('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body
+      const filter = { _id: ObjectId(id) }
 
 
-   
-    
+      const updateDoc = {
+        $set: {
+          paid: true,
+          TransitionId: payment.transactionId
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      const result1 = await paymentCollection.insertOne(payment);
+      res.send({ result: result, result1: result1 });
+
+    })
+
+    app.post('/order', async (req, res) => {
+      const ordered = (req.body.orderInfo);
+
+      const result = await orderCollection.insertOne(ordered);
+      res.send(result);
+
+    })
 
 
+    // -----------------------------for payment--------------
+    app.post("/payment", jwtToken, async (req, res) => {
+      const price = req.body
+      const amount = price.total * 100
 
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
 
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// -----------------------------
-app.post("/payment", async (req, res) => {
- 
- const price = req.body
- const amount = price.total*100 
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-
-  })
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-
-  
- 
-
- 
-})
+    })
 
 
 
+
+
+
+
+
+
+
+    app.put('/user/:id', async (req, res) => {
+      const userEmail = req.params.id;
+      const filter = { email: userEmail };
+      const options = { upsert: true };
+      const user = {
+        $set: {
+          email: userEmail
+        },
+      };
+      const result = await userCollection.updateOne(filter, user, options);
+
+      const token = jwt.sign(userEmail, process.env.ACESS_TOKEN);
+      res.send({ token: token, result: result })
+
+    })
 
 
 
@@ -187,29 +188,40 @@ app.post("/payment", async (req, res) => {
 
 
 
-        
-        // ---------------------------------
-        app.put('/user/:id', async (req, res)=>{
-            const userEmail = req.params.id;
-            const filter = { email: userEmail };
-            const options = { upsert: true };
-            const user = {
-                $set: {
-                  email: userEmail
-                },
-              };
-              const result = await userCollection.updateOne(filter, user, options);
-            console.log(userEmail);
-            const token = jwt.sign(userEmail, process.env.ACESS_TOKEN);
-            res.send({token:token, result:result})
-
-        })
 
 
-    }
-    catch(e){
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ---------------------------------
+
+
+  }
+  catch (e) {
+
+  }
 
 
 }
@@ -218,9 +230,9 @@ run().catch(console.dir);
 
 
 
-app.use('/user',productRoutes)
+app.use('/user', productRoutes)
 
 
-app.listen(port,()=>{
-    
+app.listen(port, () => {
+
 });
